c0bf66e887a23e897448f288e4ad1081f63e997c
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-09-12 12:43:37 -0700

    scaffolded basic placeholder route

--- src/routes/images/placeholder.ts
+++ src/routes/images/placeholder.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response} from 'express';
+import path from 'path';
 
 const placeholder = express.Router();
 
 placeholder.get('/', (req:Request, res:Response)=> {
-    res.send('get placeholder image');
+
+    // res.send('get placeholder image');
+    res.sendFile(path.join(__dirname, '../../images/originals/goldengate.jpg'))
 })
 
 export default placeholder;
