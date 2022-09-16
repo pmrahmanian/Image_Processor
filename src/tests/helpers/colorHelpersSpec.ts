86d612012a350722d6113b43b5785655537c4474
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-09-16 13:35:28 -0700

    bundled suites together in parent suite

--- src/tests/helpers/colorHelpersSpec.ts
+++ src/tests/helpers/colorHelpersSpec.ts
@@ -20,81 +20,83 @@ function rgbaAlphaValue() {
     }
 }}
 
+describe('Testing Color Helper Methods', ():void => {
 
-describe('Testing getRGBValue helper function', (): void => {
-    it('check output type to be number', () => {
-        expect(typeof getRGBValue('5')).toEqual('number')
+    describe('Testing getRGBValue helper function', (): void => {
+        it('check output type to be number', () => {
+            expect(typeof getRGBValue('5')).toEqual('number')
+        })
+
+        it('check return value for non numerical string', () => {
+            expect(getRGBValue('apple')).toEqual(0)
+        })
+
+        it('check return for negative input', () => {
+            expect(getRGBValue('-75')).toEqual(0)
+        })
+
+        it('check return for > 255 input', () => {
+            expect(getRGBValue('355')).toEqual(255)
+        })
+
+        it('check return for proper input', () => {
+            expect(getRGBValue('76')).toEqual(76)
+        })
     })
 
-    it('check return value for non numerical string', () => {
-        expect(getRGBValue('apple')).toEqual(0)
+    describe('Testing getRandomRGB helper function', ():void => {
+        const returnColor = getRandomRGB(1)
+        
+        it('check proper formating of rgba color object', ()=> {
+            expect(returnColor).toEqual(jasmine.objectContaining({
+                r: rgbNumber(),
+                g: rgbNumber(),
+                b: rgbNumber(),
+                alpha: rgbaAlphaValue()
+            }))
+        })
     })
 
-    it('check return for negative input', () => {
-        expect(getRGBValue('-75')).toEqual(0)
+    describe('Testing getAlphaValue helper function', ():void => {
+        
+        it('check output type to be number', () => {
+            expect(typeof getAlphaValue('0.5')).toEqual('number')
+        })
+
+        it('check return value for non numerical string', () => {
+            expect(getAlphaValue('apple')).toEqual(1)
+        })
+
+        it('check return for negative input', () => {
+            expect(getAlphaValue('-75')).toEqual(0)
+        })
+
+        it('check return for > 1 input', () => {
+            expect(getAlphaValue('355')).toEqual(1)
+        })
+
+        it('check return for proper input', () => {
+            expect(getAlphaValue('0.5')).toEqual(0.5)
+        })
     })
 
-    it('check return for > 255 input', () => {
-        expect(getRGBValue('355')).toEqual(255)
+
+    describe('Testing isLightColor helper function', ():void => {
+        it('check white', ()=> {
+            expect(isLightColor({r: 255, g: 255,b: 255, alpha:1})).toBeTrue
+        })
+
+        it('check black', ()=> {
+            expect(isLightColor({r: 0, g: 0,b: 0, alpha:1})).toBeFalse
+        })
+
+        it('check navy blue', ()=> {
+            expect(isLightColor({r: 0, g: 0,b: 100, alpha:1})).toBeFalse
+        })
+
+        it('check yellow', ()=> {
+            expect(isLightColor({r: 255, g: 255,b: 0, alpha:1})).toBeTrue
+        })
+
     })
-
-    it('check return for proper input', () => {
-        expect(getRGBValue('76')).toEqual(76)
-    })
-})
-
-describe('Testing getRandomRGB helper function', ():void => {
-    const returnColor = getRandomRGB(1)
-    
-    it('check proper formating of rgba color object', ()=> {
-        expect(returnColor).toEqual(jasmine.objectContaining({
-            r: rgbNumber(),
-            g: rgbNumber(),
-            b: rgbNumber(),
-            alpha: rgbaAlphaValue()
-        }))
-    })
-})
-
-describe('Testing getAlphaValue helper function', ():void => {
-    
-    it('check output type to be number', () => {
-        expect(typeof getAlphaValue('0.5')).toEqual('number')
-    })
-
-    it('check return value for non numerical string', () => {
-        expect(getAlphaValue('apple')).toEqual(1)
-    })
-
-    it('check return for negative input', () => {
-        expect(getAlphaValue('-75')).toEqual(0)
-    })
-
-    it('check return for > 1 input', () => {
-        expect(getAlphaValue('355')).toEqual(1)
-    })
-
-    it('check return for proper input', () => {
-        expect(getAlphaValue('0.5')).toEqual(0.5)
-    })
-})
-
-
-describe('Testing isLightColor helper function', ():void => {
-    it('check white', ()=> {
-        expect(isLightColor({r: 255, g: 255,b: 255, alpha:1})).toBeTrue
-    })
-
-    it('check black', ()=> {
-        expect(isLightColor({r: 0, g: 0,b: 0, alpha:1})).toBeFalse
-    })
-
-    it('check navy blue', ()=> {
-        expect(isLightColor({r: 0, g: 0,b: 100, alpha:1})).toBeFalse
-    })
-
-    it('check yellow', ()=> {
-        expect(isLightColor({r: 255, g: 255,b: 0, alpha:1})).toBeTrue
-    })
-
 })
