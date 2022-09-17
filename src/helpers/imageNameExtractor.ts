2a2db0ebb244cc4253ca0a47da0a7a1698e1114b
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-09-17 10:20:00 -0700

    updated conditional check

--- src/helpers/imageNameExtractor.ts
+++ src/helpers/imageNameExtractor.ts
@@ -1,5 +1,5 @@
 export default function imageNameExtractor (input:string):string {
     const fullstop:number | undefined = input.lastIndexOf('.')
-    if (fullstop) return input.slice(0,fullstop)
+    if (fullstop>0) return input.slice(0,fullstop)
     else return input
 };
