ddb3811aeae047db5ee4ffec5e1cbf524bc22ae9
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-09-17 10:22:29 -0700

    changed root endpoint to return readme file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response} from 'express';
 import routes from './routes/index'
+import path from 'path'
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req:Request, res:Response)=> {
-    res.send('connected to the root of our api')
+    // res.send('connected to the root of our api')
+    res.status(200).sendFile(path.resolve(__dirname, '../README.md'));
 })
 
 app.use('/images', routes)
